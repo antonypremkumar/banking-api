eedb0b417f9ab4a579d289328a3dc347d69807b1
Antony Premkumar <antonyashleyraja@gmail.com>
2025-02-25 12:09:25 +0100

    Included tests 1. test_transfer_funds 2. test_get_transfer_history

--- app/test_api.py
+++ app/test_api.py
@@ -48,3 +48,39 @@ def test_get_balance(client: TestClient, session: Session):
     response = client.get(f"/balance/{account_id}")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == 10000
+
+def test_transfer_funds(client: TestClient, session: Session):
+    account1_id=1
+    account2_id=5
+
+    response = client.post(f"/transfer/{account1_id}/{account2_id}/300")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "Transfer successful"}
+
+    response = client.get(f"/balance/{account1_id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == 9700
+
+    response = client.get(f"/balance/{account2_id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == 10300
+
+def test_get_transfer_history(client: TestClient, session: Session):
+    account1_id=5
+    account2_id=2
+
+    response = client.post(f"/transfer/{account1_id}/{account2_id}/500")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "Transfer successful"}
+
+    response = client.get(f"/account/{account1_id}/transfers")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+
+    assert len(data) == 2
+    assert data[0]["from_account_id"] == 1
+    assert data[0]["to_account_id"] == account1_id
+    assert data[0]["amount"] == 300
+    assert data[1]["from_account_id"] == account1_id
+    assert data[1]["to_account_id"] == account2_id
+    assert data[1]["amount"] == 500
