f827ee5e393e7d7394186cfaeae732c4b43627b3
Antony Premkumar <antonyashleyraja@gmail.com>
2025-02-25 11:03:31 +0100

    add an account to a customer

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.exceptions import HTTPException
 
 from sqlmodel import Session, select
-from database import engine
+from database import engine, initial_deposit
 import uvicorn
 
 from os import environ
-from models import CustomerBase, Customer
+from models import CustomerBase, Customer, BankAccount
 from typing import List
 
 app = FastAPI()
@@ -51,6 +51,19 @@ async def get_all_customers():
         all_categories=result.all()
         # error handling could be done here
     return all_categories
+
+# add an account to a customer
+@app.post('/account/{customer_id}', status_code=status.HTTP_201_CREATED)
+async def add_an_account(customer_id: int):
+    new_account = BankAccount(customer_id=customer_id, balance=initial_deposit)
+    with Session(engine) as session:
+        customer = session.get(Customer, customer_id)
+        if not customer:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+        session.add(new_account)
+        session.commit()
+        session.refresh(new_account)
+    return {"message": f"Added an account successfully to {customer_id}"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000)
