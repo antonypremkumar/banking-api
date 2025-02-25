bb8c6eb9b3fbe4bb58aaebd83fdc41a5fcc6b397
Antony Premkumar <antonyashleyraja@gmail.com>
2025-02-25 10:14:20 +0100

    preload a set of customers

--- app/database.py
+++ app/database.py
@@ -1,5 +1,8 @@
 from sqlmodel import SQLModel, Session, create_engine
 
+from models import Customer
+import json
+
 # Database setup
 database_name = "bank.db"
 database_url = f"sqlite:///{database_name}"
@@ -7,6 +10,17 @@ initial_deposit = 100
 
 engine = create_engine(database_url, echo=True)
 
+# Load predefined customers
+def load_faithful_customers():
+    with open("faithful_customers.json", "r") as file:
+        customers = json.load(file)
+    with Session(engine) as session:
+        for customer in customers:
+            if not session.get(Customer, customer["id"]):
+                session.add(Customer(id=customer["id"], name=customer["name"]))
+        session.commit()
+
 # create database only when run from main
 if __name__ == "__main__":
     SQLModel.metadata.create_all(engine)
+    load_faithful_customers()
